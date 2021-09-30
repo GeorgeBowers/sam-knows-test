b629193019ded17cdb5ba9527e9bb42bb3b61ba1
GeorgeBowers <george.bowers@bbc.co.uk>
2021-09-30 23:11:34 +0100

    added a MVC 'sketch'/'outline'

--- src/main/java/group/sam_knows_test/App.java
+++ src/main/java/group/sam_knows_test/App.java
@@ -1,9 +1,32 @@
 package group.sam_knows_test;
 
+import group.sam_knows_test.Model.Model;
+import group.sam_knows_test.controller.Controller;
+import group.sam_knows_test.view.View;
+
 public class App {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+//	public static void main(String[] args) throws IOException {
+//		File path = new File("outputs/");
+//
+//		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+//		ObjectMapper mapper = new ObjectMapper();
+//		mapper.setDateFormat(df);
+//		TypeFactory typeFactory = mapper.getTypeFactory();
+//
+//		for (File inputFile : path.listFiles()) {
+//			String downloadSpeedsString = Files.readString(inputFile.toPath());
+//			List<DownloadSpeed> downloadSpeedsList = mapper.readValue(downloadSpeedsString,
+//					typeFactory.constructCollectionType(List.class, DownloadSpeed.class));
+//		}
+//
+//	}
 	
+	public static void main(String[] args) {
+		Model model = new Model(args[0]);
+		View view = new View();
+		Controller controller = new Controller(model, view);
+		controller.analyseInputFiles();
+	}
+
 }
