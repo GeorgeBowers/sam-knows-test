77d60442bbe99dcb5d7c86961d6bcf4716713a8f
GeorgeBowers <george.bowers@bbc.co.uk>
2021-09-30 16:44:05 +0100

    Creation of repo

---
+++ src/main/java/group/sam_knows_test/App.java
@@ -0,0 +1,9 @@
+package group.sam_knows_test;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+}
---
+++ src/test/java/group/sam_knows_test/testing/AppTest.java
@@ -0,0 +1,12 @@
+package group.sam_knows_test.testing;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assert Boolean.TRUE;
+	}
+	
+}
